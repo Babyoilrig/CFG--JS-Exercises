94113e7973b23e846ad7a6ff8374d5411be0dcc9
babyoilrig <catrin.mentzoni@yahoo.com>
2023-02-14 21:03:37 +0000

    Some of the week 2 exercises completed

---
+++ main.js
@@ -0,0 +1,54 @@
+// //CFG JS Week 2
+// //Exercise 1.0
+// let genre = "comedy";
+// let rating = 5;
+// let pillows = 1;
+
+// if (rating == 5 && genre == "horror" && pillows == 1){
+//     console.log("Your friend will watch it")
+// }
+// else if (rating == 5 && genre == "horror" && pillows < 1){
+//     console.log("Your friend won't watch it")
+// }
+// else if (rating < 1){
+//     console.log("There's no point watching it")
+// } 
+// else if (genre !== "horror"){
+//     console.log("Your friend will watch anything")
+// }
+// else {
+//     console.log("We can all talk")
+// }
+
+//Exercise 2.1 and 2.2
+let students = ["sofia", "elizabeth", "sasha", "samantha", "abigail", "lorena", "ayesha", "adeyo", "emil"];
+
+let count = 0;
+
+for(let i = 0; i < students.length; i++){
+    count++;
+}
+console.log(count);
+//Works :)
+
+//Exercise 2.3
+let classSize = 0;
+let maxClassSize = 7;
+
+let classList = [];
+
+while(classList.length <= maxClassSize){
+    //Get random index value
+    let randomIndex = Math.floor(Math.random() *students.length);
+//Get random item
+let item = students[randomIndex];
+classList.push(item);
+}
+console.log(classList);
+//It works!!
+
+
+
+
+
+
